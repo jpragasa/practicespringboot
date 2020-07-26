671bb1bc383027939c167281d779fb32bfa803f2
jpragasa <jpragasa@seu.edu>
2020-07-25 21:53:03 -0400

    Connected with AWS

--- practice/src/main/java/com/springbootpractice/practice/dao/PersonDataAccessService.java
+++ practice/src/main/java/com/springbootpractice/practice/dao/PersonDataAccessService.java
@@ -21,14 +21,14 @@ public class PersonDataAccessService implements PersonDao
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        final String sql = "INSERT INTO person (id, name) VALUES (uuid_generate_v4(), ?)";
+        final String sql = "INSERT INTO person.person (id, name) VALUES (uuid_generate_v4(), ?)";
         jdbcTemplate.update(sql, person.getName());
         return 1;
     }
 
     @Override
     public List<Person> selectAllPeople() {
-        final String sql = "SELECT id, name FROM person";
+        final String sql = "SELECT id, name FROM person.person";
         return jdbcTemplate.query(sql, (resultSet, i) ->
         {
             UUID id = UUID.fromString(resultSet.getString("id"));
@@ -39,7 +39,7 @@ public class PersonDataAccessService implements PersonDao
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        final String sql = "SELECT id, name FROM person WHERE ID = ?";
+        final String sql = "SELECT id, name FROM person.person WHERE ID = ?";
         Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) ->
         {
             UUID personId = UUID.fromString(resultSet.getString("id"));
@@ -51,14 +51,14 @@ public class PersonDataAccessService implements PersonDao
 
     @Override
     public int deletePersonById(UUID id) {
-        final String sql = "DELETE FROM person WHERE ID = ?";
+        final String sql = "DELETE FROM person.person WHERE ID = ?";
         jdbcTemplate.update(sql, id);
         return 1;
     }
 
     @Override
     public int updatePersonById(UUID id, Person person) {
-        final String sql = "UPDATE person SET name = ? WHERE ID = ?";
+        final String sql = "UPDATE person.person SET name = ? WHERE ID = ?";
         jdbcTemplate.update(sql, person.getName(), id);
         return 1;
     }
